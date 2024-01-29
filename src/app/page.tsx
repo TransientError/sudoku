df21f80bfb60608cdd65589f43ca15ed64092c90
kvwu <kvwu@transienterror.com>
2024-01-29 05:28:47 -0800

    fix tab title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,15 @@ import App from "./app";
 
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center gap-x-5 xl:gap-x-20">
-      <App />
-    </div>
+    <html>
+      <head>
+        <title>Sudoku solve assist</title>
+      </head>
+      <body>
+        <div className="h-screen flex justify-center items-center gap-x-5 xl:gap-x-20">
+          <App />
+        </div>
+      </body>
+    </html>
   );
 }
-
